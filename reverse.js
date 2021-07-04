7a7cb2d760cf0ef1019843b36fc72e3620f97164
yaniv89 <42696262+yaniv89@users.noreply.github.com>
2021-07-04 10:57:55 +0300

    reversed text for figma

---
+++ reverse.js
@@ -0,0 +1,29 @@
+function reverseString(str) {
+    var english = /^[A-Za-z0-9]*$/;
+    var words = str.split(" ");
+    let newText = "";
+    for (let i = words.length - 1; i >= 0; i--){
+        var word = words[i];
+        if(english.test(word) || !isNaN(parseFloat(word))){
+            var joinString = word;
+        }
+        else{
+            var word = words[i].split("");
+            reversedWord = word.reverse();
+            var joinString = reversedWord.join('');
+        }
+        
+        newText += " " + joinString;
+
+    }
+
+    return newText;
+}
+
+
+function initReverse() {
+    var str = $('textarea').val();
+    var splitText = str.split("\n");
+    var finalText = splitText.map(x=>x=reverseString(x));
+    $("textarea").val(finalText.join("\n"));
+}
