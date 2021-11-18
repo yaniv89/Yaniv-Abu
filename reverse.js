522ce1f7ccc25339fe1ad431b3687de2fca8659f
Yaniv Abu <abu.yaniv@simplex-ltd.com>
2021-11-18 09:35:58 +0200

    updated

--- reverse.js
+++ reverse.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function reverseString(str) {
     var english = /^[A-Za-z0-9]*$/;
     var words = str.split(" ");
@@ -20,7 +19,7 @@ function reverseString(str) {
 			newText += " " + joinString;
 		}
 
-    }
+    } 
 
     return newText;
 }
@@ -31,34 +30,4 @@ function initReverse() {
     var splitText = str.split("\n");
     var finalText = splitText.map(x=>x=reverseString(x));
     $("textarea").val(finalText.join("\n"));
-=======
-function reverseString(str) {
-    var english = /^[A-Za-z0-9]*$/;
-    var words = str.split(" ");
-    let newText = "";
-    for (let i = words.length - 1; i >= 0; i--){
-        var word = words[i];
-        if(english.test(word) || !isNaN(parseFloat(word))){
-            var joinString = word;
-        }
-        else{
-            var word = words[i].split("");
-            reversedWord = word.reverse();
-            var joinString = reversedWord.join('');
-        }
-        
-        newText += " " + joinString;
-
-    }
-
-    return newText;
-}
-
-
-function initReverse() {
-    var str = $('textarea').val();
-    var splitText = str.split("\n");
-    var finalText = splitText.map(x=>x=reverseString(x));
-    $("textarea").val(finalText.join("\n"));
->>>>>>> 7a7cb2d760cf0ef1019843b36fc72e3620f97164
 }
